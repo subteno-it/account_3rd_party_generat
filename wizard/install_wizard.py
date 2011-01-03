a384550343cbd6e90bb9f3c1a301a4df646dd3e3
Christophe Chauvet <christophe.chauvet@syleam.fr>
2011-01-03 23:24:15 +0100

    [FIX] PEP8 Compliant

--- wizard/install_wizard.py
+++ wizard/install_wizard.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution    
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2009 SISTHEO
+#                  2010-2011 Christophe Chauvet <christophe.chauvet@syleam.fr>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -19,26 +19,27 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import fields,osv
+
+from osv import osv
+from osv import fields
 import types
 
 
 class wizard_install_third_part_accounts(osv.osv_memory):
     """
     """
-    _name='wizard.install.third.part.accounts'
+    _name = 'wizard.install.third.part.accounts'
 
     _columns = {
         'company_id': fields.many2one('res.company', 'Company', required=True),
-        'receivable_id': fields.many2one('account.account', 'Account receivable', domain="[('type', '=', 'view')]", required=True),     #,('user_type','=','receivable')
+        'receivable_id': fields.many2one('account.account', 'Account receivable', domain="[('type', '=', 'view')]", required=True),
         'payable_id': fields.many2one('account.account', 'Account payable', domain="[('type', '=', 'view')]", required=True),
     }
 
-
     def _default_account_id(self, cr, uid, account_type, context={}):
-        account_type_id = self.pool.get('account.account.type').search(cr, uid, [('code','=',account_type)])    #, context
-        srch_args = [('type', '=', 'view'),('user_type','in',account_type_id)]
-        account_id = self.pool.get('account.account').search(cr, uid, srch_args)    #[0]    #, limit=1, order='code')
+        account_type_id = self.pool.get('account.account.type').search(cr, uid, [('code', '=', account_type)])
+        srch_args = [('type', '=', 'view'), ('user_type', 'in', account_type_id)]
+        account_id = self.pool.get('account.account').search(cr, uid, srch_args)
         if account_id:
             if type(account_id) is types.IntType:
                 return account_id
@@ -46,43 +47,41 @@ class wizard_install_third_part_accounts(osv.osv_memory):
                 return account_id[0]
         return False
 
-
     def _default_receivable_id(self, cr, uid, context={}):
-        receivable_id = self._default_account_id(cr, uid,'receivable', context)
+        receivable_id = self._default_account_id(cr, uid, 'receivable', context)
         return receivable_id
 
     def _default_payable_id(self, cr, uid, context={}):
         payable_id = self._default_account_id(cr, uid, 'payable', context)
         return payable_id
 
-
     _defaults = {
-        'company_id': lambda self, cr, uid, c: self.pool.get('res.users').browse(cr,uid,[uid],c)[0].company_id.id,
+        'company_id': lambda self, cr, uid, c: self.pool.get('res.users').browse(cr, uid, [uid], c)[0].company_id.id,
         'receivable_id': _default_receivable_id,
         'payable_id': _default_payable_id,
     }
 
-
     def _set_property(self, cr, uid, prop_name, prop_account_id, company_id):
-        """ Set/Reset default properties
+        """
+        Set/Reset default properties
         """
         property_obj = self.pool.get('ir.property')
-        prp_ids = property_obj.search(cr, uid, [('name','=', prop_name ),('company_id','=',company_id)])
-#FIXME Add critéria to find only 1 record
+        prp_ids = property_obj.search(cr, uid, [('name', '=', prop_name), ('company_id', '=', company_id)])
+        # FIXME Add critéria to find only 1 record
         if prp_ids:
-            if len(prp_ids) == 1:   #the property exist: modify it
+            if len(prp_ids) == 1:  # the property exist: modify it
                 vals = {
                     'value': prop_account_id and 'account.account,' + str(prop_account_id) or False,
                 }
                 out_id = prp_ids[0]
                 property_obj.write(cr, uid, [out_id], vals)
             else:
-#FIXME Over write the nly record that have res = NULL
+                #FIXME Over write the nly record that have res = NULL
                 out_id = False
                 pass    # DO NOTHING / DO NOT CHANGE EXISTING DATAS
-        else:   #create the property
+        else:  # create the property
             fields_obj = self.pool.get('ir.model.fields')
-            field_ids = fields_obj.search(cr, uid, [('name','=',prop_name),('model','=','res.partner'),('relation','=','account.account')])
+            field_ids = fields_obj.search(cr, uid, [('name', '=', prop_name), ('model', '=', 'res.partner'), ('relation', '=', 'account.account')])
             vals = {
                 'name': prop_name,
                 'company_id': company_id,
@@ -91,7 +90,6 @@ class wizard_install_third_part_accounts(osv.osv_memory):
             }
             out_id = property_obj.create(cr, uid, vals)
         return out_id
-
 
     def action_start_install(self, cr, uid, ids, context=None):
         """ Create the properties : specify default account (payable and receivable) for partners
@@ -106,15 +104,13 @@ class wizard_install_third_part_accounts(osv.osv_memory):
             'res_model': 'ir.actions.configuration.wizard',
             'view_type': 'form',
             'view_mode': 'form',
-            'target':'new',
+            'target': 'new',
         }
         return next_action
 
-
-    def action_cancel(self,cr,uid,ids,conect=None):
-        return {'type':'ir.actions.act_window_close' }
+    def action_cancel(self, cr, uid, ids, conect=None):
+        return {'type': 'ir.actions.act_window_close'}
 
 wizard_install_third_part_accounts()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
