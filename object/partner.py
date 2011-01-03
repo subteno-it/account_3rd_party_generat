dddce70adcdd451ba7e3ad0361c59cae826b746e
Christophe Chauvet <christophe.chauvet@syleam.fr>
2011-01-03 22:54:30 +0100

    [IMP] PEP8 Compliant [FIX] DocString

--- object/partner.py
+++ object/partner.py
@@ -3,6 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2009 SISTHEO
+#                  2010-2011 Christophe Chauvet <christophe.chauvet@syleam.fr>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -41,16 +42,19 @@ class res_partner(osv.osv):
         'customer': lambda *a: 0,   # Do not compute account number if not necessary
     }
 
-#----------------------------------------------------------
-#   Private methods C&S
-#----------------------------------------------------------
+    #----------------------------------------------------------
+    #   Private methods C&S
+    #----------------------------------------------------------
     def _get_compute_account_number(self, cr, uid, partner, seq_patern):
         """Compute account code based on partner and sequence
 
-        @param partner: (browse/id) partner's datas
-        @param seq_patern: (string) the sequence witch will be use as a patern/template
+        :param partner: current partner
+        :type  partner: osv.osv.browse
+        :param seq_patern: the sequence witch will be use as a pattern/template
+        :type  seq_patern: str
 
-        @return: (string) the account code/number
+        :return: the account code/number
+        :rtype: str
         """
         if seq_patern.find('{') >= 0:
             prefix = seq_patern[:seq_patern.index('{')]
@@ -58,10 +62,8 @@ class res_partner(osv.osv):
             body = seq_patern[len(prefix)+1:][:len(seq_patern)-len(prefix)-len(suffix)-2]
 
             ar_args = body.split('|')
-    #        print "DEBUG: compute_account_number ar_args = %r" % ar_args
             # partner field is always first
             partner_value = getattr(partner, ar_args[0])
-    #        print "DEBUG: compute_account_number partner_value = %r" % partner_value
             if partner_value:
                 # Modificators
                 mdf = Modificator(partner_value)
@@ -90,12 +92,13 @@ class res_partner(osv.osv):
     def _get_account_model(self, cr, uid, model_name):
         """Retreive default property for partner's account. This will be use as template / default values.
 
-        @param model_name: (string) the name of the property
-
-        @return: (browse/id) the account (object)
+        :param model_name: the name of the property
+        :type  model_name: str
+        :return: the account (object)
+        :rtype: osv.osv.browse
         """
         property_obj = self.pool.get('ir.property')
-#FIXME OpenERP is creating bad properties with res_id setted, so by luck we can retreive the original one
+        #FIXME OpenERP is creating bad properties with res_id setted, so by luck we can retreive the original one
         property_id = property_obj.search(cr, uid, [('name','=', model_name),('res_id','=',False)])
         if not property_id or len(property_id) != 1:
             #raise osv.except_osv(_('Error !'),_('You need to define ONE default account number in properties for %s' % model_name))
@@ -107,41 +110,43 @@ class res_partner(osv.osv):
 
 
     def _get_data_account_model(self, brobj_account):
-        """Retreive account values from template.
-
-        @param brobj_account: (browse/id) account template datas/values
-
-        @return: (dict) the account values
         """
-#FIXME there may be a better way to transfert data form parent to child's patern
+        Retreive account values from template.
+
+        :param brobj_account: account template datas/values
+        :type  brobj_account: osv.osv.browse
+        :return: the account values
+        :rtype: dict
+        """
+        #FIXME there may be a better way to transfert data form parent to child's pattern
         data_account_model = {
-                                    'currency_id': brobj_account.currency_id.id,
-                                    'user_type': brobj_account.user_type.id,
-                                    'parent_id': brobj_account.id,
-                                    'reconcile': brobj_account.reconcile,
-                                    'shortcut': brobj_account.shortcut,
-#                                    'tax_ids': parent_account.,        # not a good idea to have tax hear
-#                                    'note': parent_account,            # keep it blank
-                                    'company_currency_id': brobj_account.company_currency_id.id,
-                                    'company_id': brobj_account.company_id.id,
-                                    'active': True,
-                                    'parent_left': brobj_account.parent_left,
-                                    'parent_right': brobj_account.parent_right,
-                                    'currency_mode': brobj_account.currency_mode,
-                                    'check_history': brobj_account.check_history,
+            'currency_id': brobj_account.currency_id.id,
+            'user_type': brobj_account.user_type.id,
+            'parent_id': brobj_account.id,
+            'reconcile': brobj_account.reconcile,
+            'shortcut': brobj_account.shortcut,
+            'company_currency_id': brobj_account.company_currency_id.id,
+            'company_id': brobj_account.company_id.id,
+            'active': True,
+            'parent_left': brobj_account.parent_left,
+            'parent_right': brobj_account.parent_right,
+            'currency_mode': brobj_account.currency_mode,
+            'check_history': brobj_account.check_history,
         }
-        #print "_get_data_account_model %r" % data_account_model
         return data_account_model
 
 
     def _get_account_sequence(self, cr, uid, seq_name):
-        """Retreive sequence value (next number).
-
-        @param seq_name: (string) name of the sequence (not the same on suppliers and customers)
-
-        @return: (string) the next value from selected sequence
         """
-#FIXME code field is type of sequence nor code, so we're oblige to search on the name (witch can be translated)
+        Retrieve sequence value (next number)
+
+        :param seq_name: Name of the sequence (not the same on suppliers and customers)
+        :type  seq_name: str
+
+        :return: The next value from selected sequence
+        :rtype: str
+        """
+        #FIXME code field is type of sequence nor code, so we're oblige to search on the name (witch can be translated)
         sequence_obj = self.pool.get('ir.sequence')
         sequence_id = sequence_obj.search(cr, uid, [('code','=', 'account.partner.third_part'), ('name','=', seq_name)])
         if not sequence_id or len(sequence_id) != 1:
@@ -150,13 +155,15 @@ class res_partner(osv.osv):
         return sequence_data
 
 
-#----------------------------------------------------------
-#   Private methods CUSTOMER ONLY
-#----------------------------------------------------------
+    #----------------------------------------------------------
+    #   Private methods CUSTOMER ONLY
+    #----------------------------------------------------------
     def _get_customer_account_model(self, cr, uid):
-        """Retreive values from template for customer account.
+        """
+        Retrieve values from template for customer account.
 
-        @return: (dict) the account values
+        :return: The account values
+        :rtype: dict
         """
         parent_account = self._get_account_model(cr, uid, 'property_account_receivable')
         if not parent_account:
@@ -167,20 +174,24 @@ class res_partner(osv.osv):
 
 
     def _get_customer_account_sequence(self, cr, uid):
-        """Retreive customer sequence next value.
+        """
+        Retrieve customer sequence next value.
 
-        @return: (string) the next value from selected sequence
+        :return: The next value from selected sequence
+        :rtype: str
         """
         return self._get_account_sequence(cr, uid, 'Customer account')
 
 
-#----------------------------------------------------------
-#   Private methods SUPPLIER ONLY
-#----------------------------------------------------------
+    #----------------------------------------------------------
+    #   Private methods SUPPLIER ONLY
+    #----------------------------------------------------------
     def _get_supplier_account_model(self, cr, uid):
-        """Retreive values from template for supplier account.
+        """
+        Retrieve values from template for supplier account.
 
-        @return: (dict) the account values
+        :return: The account values
+        :rtype: dict
         """
         parent_account = self._get_account_model(cr, uid, 'property_account_payable')
         if not parent_account:
@@ -191,16 +202,18 @@ class res_partner(osv.osv):
 
 
     def _get_supplier_account_sequence(self, cr, uid):
-        """Retreive supplier sequence next value.
+        """
+        Retrieve supplier sequence next value.
 
-        @return: (string) the next value from selected sequence
+        :return: The next value from selected sequence
+        :rtype: str
         """
         return self._get_account_sequence(cr, uid, 'Supplier account')
 
 
-# #########################################################
-#       O V E R L O A D     O S V
-# #########################################################
+    # #########################################################
+    #       O V E R L O A D     O S V
+    # #########################################################
     def create(self, cr, uid, data, context={}):
         new_id = super(res_partner, self).create(cr, uid, data, context)
         if (('customer' in data) and (data['customer'] == 1) and not context.get('skip_account_customer',False)) or (('supplier' in data) and (data['supplier'] == 1) and not context.get('skip_account_supplier',False)):
@@ -212,9 +225,9 @@ class res_partner(osv.osv):
         # Update all ids (batch way)
         osv_stuff = super(res_partner, self).write(cr, uid, ids, vals, context)
         for id in ids:
-        # Update one by one
+            # Update one by one
             partner = self.browse(cr, uid, [id])[0]
-        # Customer account number
+            # Customer account number
             default_receivable_account = self._get_account_model(cr, uid, 'property_account_receivable')
             if default_receivable_account and partner.customer and (partner.property_account_receivable.id == default_receivable_account.id) and not context.get('skip_account_customer',False):
                 account_patern = self._get_customer_account_model(cr, uid)
@@ -225,7 +238,7 @@ class res_partner(osv.osv):
                 debug(account_patern)
                 customer_account_id = self.pool.get('account.account').create(cr, uid, account_patern)
                 super(res_partner, self).write(cr, uid, id, {'property_account_receivable': customer_account_id} )
-        # Supplier account number
+            # Supplier account number
             default_payable_account = self._get_account_model(cr, uid, 'property_account_payable')
             if default_payable_account and partner.supplier and (partner.property_account_payable.id == default_payable_account.id) and not context.get('skip_account_supplier',False):
                 account_patern = self._get_supplier_account_model(cr, uid)
